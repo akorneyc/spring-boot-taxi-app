8dce76d95cac845c893f3f811c38106cf035b065
Alexey Korneychuk <akorneychuk@gomel.iba.by>
2020-05-14 15:03:08 +0300

    added hystrix

--- taxi-clients/src/main/java/io/akorneyc/taxiclients/TaxiClientsApplication.java
+++ taxi-clients/src/main/java/io/akorneyc/taxiclients/TaxiClientsApplication.java
@@ -2,12 +2,15 @@ package io.akorneyc.taxiclients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class TaxiClientsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaxiClientsApplication.class, args);
 	}
-
 }
--- taxi-control/src/main/java/io/akorneyc/taxicontrol/TaxiControlApplication.java
+++ taxi-control/src/main/java/io/akorneyc/taxicontrol/TaxiControlApplication.java
@@ -2,8 +2,12 @@ package io.akorneyc.taxicontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class TaxiControlApplication {
 
 	public static void main(String[] args) {
